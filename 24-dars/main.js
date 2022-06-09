98c80c6bb4af85798b28d1380b498c1c7a7e1841
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2022-06-09 16:33:03 +0500

    lesson 24

---
+++ 24-dars/main.js
@@ -0,0 +1,52 @@
+// let ismFamiliya = "Diyorbek Olimov";
+/* let age = undefined;
+let age = 21;
+age = "maktab";
+let isWinner = true; */
+
+// console.log(ismFamiliya);
+// console.log(typeof isWinner);
+// console.log(typeof age);
+
+// Arifmetik operatorlar
+
+let son1 = 24,
+  son2 = 6;
+
+// console.log(son1 + son2); // 30
+// console.log(son1 - son2); // 18
+// console.log(son1 * son2); // 144
+// console.log(son1 / son2); // 24 : 6 = 4
+// console.log(son1 % son2); // qoldiqni topish uchun, natija: 0 (modulo)
+// console.log(son1 ** son2); // 24 ni 6-darajasini qaytaradi
+
+// console.log(son1++); // post-increment, son1 = son1 + 1
+// console.log("Tekshirish uchun:", son1)
+// console.log(++son2); // pre-increment   son1 = son1 + 1
+// console.log(son1--); // post-decrement  son1 = son1 - 1
+// console.log(--son1); // pre-decrement   son1 = son1 - 1
+
+// Comparison operatorlar
+
+let son3 = "10",
+  son4 = 10;
+
+// console.log(son3 == son4);
+// console.log(son3 === son4);
+
+// console.log(son3 >= son4) // true
+// console.log(son3 <= son4) // false
+// console.log(son3 == son4) // false
+// console.log(son3 != son4) // true
+// console.log(son3 > son4) // true
+// console.log(son3 < son4) // false
+
+// Ternary operators
+
+let son5 = 2006;
+
+let pasport =
+  son5 < 2005 ? "Fuqarolik pasporti olgan!" : "Fuqarolik pasporti olmagan!";
+// pasport = "Fuqarolik pasporti olgan!"
+
+console.log(pasport);
