cb76a80aa2dda3eb90c37153fea5a6b5b360f28b
Diyorbek <diyorbekolimov2000@gmail.com>
2022-08-23 12:38:53 +0500

    react app

---
+++ 48-dars/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+// import test from "./test";
+import { test2, test3, test4 } from "./test";
+import Button from "./Button";
+
+function App() {
+  console.log(test2, test3, test4);
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
+// SPA - Single Page Application - Bir sahifali ilova
---
+++ 48-dars/src/test.js
@@ -0,0 +1,10 @@
+// const test = "TEST";
+// const test1 = "TEST";
+// export const test2 = "TEST 2";
+// export const test3 = "TEST 3";
+// export const test4 = "TEST 4";
+
+// export default test; // default export
+// export default test; // default export
+
+// export { test };
